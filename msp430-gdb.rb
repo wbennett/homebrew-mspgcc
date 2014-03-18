1f309bb2b880b8521e462449bb7ec2d01fea971a
Paul Bennett <wm.paul.bennett@gmail.com>
2014-03-17 20:06:59 -0500

    updating compiler flags

--- msp430-gdb.rb
+++ msp430-gdb.rb
@@ -23,7 +23,7 @@ class Msp430Gdb < Formula
   def install
     mkdir 'build' do
         #supress warnings
-        ENV['CFLAGS']='-Wno-error=return-type -Wno-error=sizeof-pointer-memaccess'
+        ENV['CFLAGS']='-Wno-error=return-type -Wno-error=sizeof-pointer-memaccess -Wno-error=sometimes-uninitialized'
         system "../configure",
                 "--target=msp430",
                 "--program-prefix='msp430-'",
