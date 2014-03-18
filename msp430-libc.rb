a8f49214d385c1ef72545ed4df2f67fcc401a2e7
Paul Bennett <wm.paul.bennett@gmail.com>
2014-03-17 20:18:16 -0500

    fixing the depends on references

--- msp430-libc.rb
+++ msp430-libc.rb
@@ -5,16 +5,16 @@ class Msp430Libc < Formula
   url 'https://sourceforge.net/projects/mspgcc/files/msp430-libc/msp430-libc-20120716.tar.bz2'
   sha1 '2ab5be8e645a5e96f64de9dd45ce96d129180ed1'
 
-  depends_on 'msp430-gcc'
-  depends_on 'msp430-mcu'
+  depends_on 'wbennett/mspgcc/msp430-gcc'
+  depends_on 'wbennett/mspgcc/msp430-mcu'
 
   def install
     # When building, we need to have the "msp430/bin" directory on the path.
     # This is because, even when we invoke msp430-gcc, it goes looking for a
     # binary called "as". If we don't do this, then it will find the system
     # (i.e., x86) as.
-    binutils = Formula.factory('msp430-binutils')
-    gcc = Formula.factory('msp430-gcc')
+    binutils = Formula.factory('wbennett/mspgcc/msp430-binutils')
+    gcc = Formula.factory('wbennett/mspgcc/msp430-gcc')
     msppath = "#{binutils.opt_prefix}/msp430/bin:#{gcc.opt_prefix}/msp430/bin"
     Dir.chdir 'src' do
       system "env", "PATH=#{msppath}:#{ENV['PATH']}", "make"
