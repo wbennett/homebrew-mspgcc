18fca1b995e3e4d6979fb647487e79b35e3143bb
Paul Bennett <wm.paul.bennett@gmail.com>
2014-03-17 20:26:00 -0500

    unintialized constant

--- mspdebug.rb
+++ mspdebug.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class MspDebug < Formula
+class MspDebugFormula < Formula
     homepage 'http://mspdebug.sourceforge.net'
     url 'http://sourceforge.net/projects/mspdebug/files/mspdebug-0.21.tar.gz'
     sha1 'a439acd166e90bfd19ddf2c3459eee9643e55368'
